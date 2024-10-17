af6d6d871453d84d769f84769d10acbb1e98086b
Rodney Neville <heptarodney@creasp.org.br>
2024-10-17 08:57:31 -0300

    cores

---
+++ data_transformations/election_correspondency/ingest.py
@@ -0,0 +1,119 @@
+import pandas as pd
+from typing import List
+from bs4 import BeautifulSoup
+import requests
+import os
+import zipfile
+import shutil
+
+import logging
+from typing import List
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import split, col, to_date, lit, to_timestamp, lag, lead, when
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+# Função para baixar o arquivo
+def prepare_files(params, input_path):
+    param = params.split('_')
+    #1t_BA_061020241406
+    uf_list = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','TO','ZZ']
+    uf_list = ['AC','AL']
+    for uf in uf_list:
+        uf_filename = f"CESP_{param[0]}t_{uf}_{param[1]}.zip"
+        url_download = "https://cdn.tse.jus.br/estatistica/sead/eleicoes/eleicoes2024/correspesp/"
+        zip_filepath = os.path.join(input_path, uf_filename)
+        
+        # Download do arquivo
+        try:
+            logging.info(f"Baixando o arquivo {uf_filename}...")
+            download_file(url_download + uf_filename, input_path, uf_filename)
+        except Exception as e:
+            logging.error(f"Erro ao baixar o arquivo {uf_filename}: {e}")
+            
+ 
+        # Descompactar o arquivo
+        try:
+            logging.info(f"Descompactando o arquivo {uf_filename}...")
+            unzip_file(zip_filepath, input_path + "/unzipped" ,'csv')
+        except Exception as e:
+            logging.error(f"Erro ao descompactar o arquivo {uf_filename}: {e}")
+             
+
+
+
+def download_file(linkFile,path,file_name):
+    current_directory = os.getcwd()
+    file_path = os.path.join(current_directory, path,file_name)
+    
+
+    if os.path.exists(file_path):
+        return True
+        
+    print(f"Baixando { file_name}...")
+
+    response = requests.get(linkFile, stream=True)
+
+   
+    with open(file_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):  # Definido para baixar em pedaços de 8KB
+            f.write(chunk)
+                
+    if(os.path.exists(file_path)):
+        return True
+    else:
+        return False
+
+
+
+def unzip_file(zip_path, extract_to, filter='jez'):
+    print(f"Extraindo arquivos...")
+
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        for file_info in zip_ref.infolist():
+            # Verifica se a extensão do arquivo corresponde ao filtro
+            if file_info.filename.endswith(filter):
+                # Extrai apenas os arquivos que correspondem ao filtro de extensão
+                zip_ref.extract(file_info, extract_to)
+
+    
+    if(os.path.exists(extract_to)):
+        return True
+    else:
+        return False
+    
+
+
+def run(spark: SparkSession, ingest_path: str, output_path: str) -> int:
+    print(f"Gerando DataFrame no Spark...")
+
+    df = spark.read.csv(ingest_path, sep=';', header=True, inferSchema=True,  encoding='latin1')
+
+    # Filtrar os eventos que correspondem a "Eleitor foi habilitado" e "O voto do eleitor foi computado"
+    df_filtered = df.select("AA_ELEICAO", "CD_PLEITO", "SG_UF", "CD_MUNICIPIO", "NM_MUNICIPIO", "NR_ZONA", "NR_URNA_ESPERADA")
+
+    df_final = df_filtered.dropDuplicates(["AA_ELEICAO", "CD_PLEITO", "SG_UF", "CD_MUNICIPIO", "NM_MUNICIPIO", "NR_ZONA", "NR_URNA_ESPERADA"])
+
+   
+    df_final.repartition(1).write.mode("overwrite").option("header", "true").csv(output_path + "/csv")
+
+    return df_final.count()
+
+
+# Função para limpar arquivos temporários
+def clean_up(input_path):
+#    current_directory = os.getcwd()
+    
+    try:
+        #shutil.rmtree(os.path.join(input_path, "unzipped"))
+        shutil.rmtree(input_path)
+        #os.remove(file_path)
+
+        logging.info("Arquivos temporários removidos com sucesso.")
+    except Exception as e:
+        logging.error(f"Erro ao remover arquivos temporários: {e}")
+
+    if not os.path.exists(input_path):
+        os.makedirs(input_path)
--- jobs/election_correspondency.py
+++ jobs/election_correspondency.py
@@ -7,7 +7,7 @@ import sys
 from pyspark.sql import SparkSession
 
 
-from data_transformations.election import ingest
+from data_transformations.election_correspondency import ingest
 
 LOG_FILENAME = 'project.log'
 APP_NAME = "Election Pipeline: Ingest Corresponcency"
@@ -16,73 +16,12 @@ APP_NAME = "Election Pipeline: Ingest Corresponcency"
 def setup_logging():
     logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
-# Função para baixar o arquivo
-def download_and_unzip(params, input_path):
-    param = params.split('_')
-    #1t_BA_061020241406
-    uf_list = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','TO','ZZ']
-    uf_list = ['AC','AL']
-    for uf in uf_list:
-        uf_filename = f"CESP_{param[0]}t_{uf}_{param[1]}.zip"
-        url_download = "https://cdn.tse.jus.br/estatistica/sead/eleicoes/eleicoes2024/correspesp/"
-        zip_filepath = os.path.join(input_path, uf_filename)
-        
-        # Download do arquivo
-        try:
-            logging.info(f"Baixando o arquivo {uf_filename}...")
-            ingest.download_file(url_download + uf_filename, input_path, uf_filename)
-        except Exception as e:
-            logging.error(f"Erro ao baixar o arquivo {uf_filename}: {e}")
-            sys.exit(1)
- 
-        # Descompactar o arquivo
-        try:
-            logging.info(f"Descompactando o arquivo {uf_filename}...")
-            ingest.unzip_file(zip_filepath, input_path + "/unzipped" ,'csv')
-        except Exception as e:
-            logging.error(f"Erro ao descompactar o arquivo {uf_filename}: {e}")
-            sys.exit(1) 
-
-# Função para processar os logs extraídos
-def process_extracted_logs(input_path):
-    logs_dir = input_path +  "/logs"
-    unzipped_dir = input_path + "/unzipped"
-    
-    #logging.error(f"Extraindo logs {unzipped_dir} para {logs_dir}")
-    
-    if not os.path.exists(logs_dir):
-        os.makedirs(logs_dir)
-    
-    # Iterar sobre os arquivos descompactados
-    for filename in os.listdir(unzipped_dir):
-        file_path = unzipped_dir +"/"+ filename
-        if os.path.isfile(file_path):
-            try:
-                ingest.extract_log_text(file_path, logs_dir)
-            except Exception as e:
-                logging.error(f"Erro ao extrair o arquivo {file_path}: {e}")
-
-
-# Função para limpar arquivos temporários
-def clean_up(input_path):
-    current_directory = os.getcwd()
-    file_path = os.path.join(current_directory, input_path)
-    
-    try:
-        #shutil.rmtree(os.path.join(input_path, "unzipped"))
-        #shutil.rmtree(os.path.join(input_path, "logs"))
-        os.remove(file_path)
-
-        logging.info("Arquivos temporários removidos com sucesso.")
-    except Exception as e:
-        logging.error(f"Erro ao remover arquivos temporários: {e}")
-
 
 def generate_parquet(input_path, output_path):
 
     spark = SparkSession.builder.appName(APP_NAME).getOrCreate()
    
-    ingest.run(spark, input_path, output_path)
+    ingest.run(spark, input_path + "/unzipped/*.csv", output_path)
     spark.stop()
 
 
@@ -97,11 +36,13 @@ def ler(input_csv_path):
 # Função principal
 def main(input_path, output_path, params):
     logging.info("Aplicação inicializada: " + APP_NAME)
+    
     #ler("out/")
-    download_and_unzip(params, input_path)
-   # process_extracted_logs(input_path)
-   # generate_parquet(input_path+"/logs", output_path)
-    clean_up(input_path)
+    ingest.prepare_files(params, input_path)
+    
+    generate_parquet(input_path,output_path)
+
+    ingest.clean_up(input_path)
     
     logging.info("Aplicação finalizada: " + APP_NAME)
 
@@ -110,7 +51,7 @@ if __name__ == '__main__':
     setup_logging()
     
     if len(sys.argv) != 4:
-        logging.warning("Caminho de entrada, caminho de saída e UF são obrigatórios.")
+        logging.warning("Caminho de entrada, caminho de saída e Parâmetros são obrigatórios.")
         sys.exit(1)
    
     input_path = sys.argv[1]
@@ -118,3 +59,4 @@ if __name__ == '__main__':
     uf = sys.argv[3]
 
     main(input_path, output_path, uf)
+
